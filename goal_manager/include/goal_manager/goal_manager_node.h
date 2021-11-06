f988eb609ab24e3d15d18db58404ba263e8c36af
Taishi Ueda <taishi.ueda@roms.inc>
2021-11-06 23:31:05 +0900

    Complete

--- goal_manager/include/goal_manager/goal_manager_node.h
+++ goal_manager/include/goal_manager/goal_manager_node.h
@@ -7,7 +7,13 @@
 
 #ifndef GOAL_MANAGER_HPP
 #define GOAL_MANAGER_HPP
+#include <stdint.h>
+#include <random>
+#include <memory>
 #include <ros/ros.h>
+#include <geometry_msgs/PoseWithCovarianceStamped.h>
+#include <geometry_msgs/PoseStamped.h>
+#include <actionlib_msgs/GoalStatusArray.h>
 
 class GoalManager{
     public:
@@ -20,8 +26,41 @@ class GoalManager{
     private:
     ros::NodeHandle nh_;
     ros::Timer timer_;
-    ros::Publisher pub_;
+    ros::Publisher pub_init_pose_;
+    ros::Publisher pub_target_;
+    ros::Subscriber sub_status_;
+
+    //! for generating randomized target pose.
+    std::unique_ptr<std::uniform_real_distribution<double>> unif_ptr_;
+    std::default_random_engine rnd_engine_;
+
+    double timeout_s_{30.0};
+    double wait_after_new_target_s_{10.0};
+    double init_w_deg_{0.0};
+    double target_center_x_m_{0.0};
+    double target_center_y_m_{0.0};
+    double target_radius_m_{2.0};
+    double last_published_s_{0.0};
+    uint8_t nav_status_{0u};
+    bool is_first_{true};
+    geometry_msgs::PoseWithCovarianceStamped init_pose_;
+
+    /**
+     * generate target pose for mobe_base.
+     * \param[out] target geterated target.
+     *
+     * a target is generated on a circle of radius set by target_radius_m_,
+     *     and center point set by target_center_x_m_ and target_center_y_m_.
+     */
+    void generateTarget(geometry_msgs::PoseStamped& target);
+
+    //! publish initial pose and target pose continuously according to goal status.
     void timerCallback(const ros::TimerEvent& e);
+
+    //! update goal status.
+    void navStatusCallBack(const actionlib_msgs::GoalStatusArray::ConstPtr& status);
+
+    void publishTarget();
 };
 
 #endif /* !GOAL_MANAGER_HPP */
--- goal_manager/src/goal_manager_node.cpp
+++ goal_manager/src/goal_manager_node.cpp
@@ -4,10 +4,12 @@
  *
  */
 
-#include <nav_msgs/MoveBaseAction.h>
+#include <vector>
+#include <move_base_msgs/MoveBaseAction.h>
 #include "goal_manager/goal_manager_node.h"
 
 GoalManager::GoalManager(){
+    unif_ptr_.reset(new std::uniform_real_distribution<double>(-M_PI, M_PI));
     bool is_ok = init();
     ROS_ASSERT(is_ok);
 }
@@ -16,15 +18,111 @@ GoalManager::~GoalManager(){
 }
 
 bool GoalManager::init(){
+
+    //--- create initial pose ---
+    // header
+    init_pose_.header.seq = 0;
+    nh_.param("map_frame", init_pose_.header.frame_id, std::string("map"));
+    // timeout
+    nh_.param("timeout_s", timeout_s_, 30.0);
+    // position
+    nh_.param("initial_pose_x_m", init_pose_.pose.pose.position.x, -2.0);
+    nh_.param("initial_pose_y_m", init_pose_.pose.pose.position.y, -0.5);
+    init_pose_.pose.pose.position.z = 0.0;
+    // orientation
+    init_w_deg_ = 0.0;
+    nh_.param("initial_pose_w_deg", init_w_deg_, 0.0);
+    init_pose_.pose.pose.orientation.x = 0.0;
+    init_pose_.pose.pose.orientation.y = 0.0;
+    init_pose_.pose.pose.orientation.z = sin(init_w_deg_/180.0*M_PI/2.0);
+    init_pose_.pose.pose.orientation.w = cos(init_w_deg_/180.0*M_PI/2.0);
+    // covariance. same as values published by rviz.
+    init_pose_.pose.covariance = {
+	0.25, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.25, 0.0,
+	0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0,
+	0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0,
+	0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.06853};
+
+    // parameters for generateTarget.
+    nh_.param("target_center_x_m", target_center_x_m_, 0.0);
+    nh_.param("target_center_y_m", target_center_y_m_, 0.0);
+    nh_.param("target_radius_m", target_radius_m_, 2.0);
+
+    // create instants
+    pub_init_pose_  = nh_.advertise<geometry_msgs::PoseWithCovarianceStamped>(
+	    "/initialpose", 10);
+    pub_target_  = nh_.advertise<geometry_msgs::PoseStamped>(
+	    "/move_base_simple/goal", 10);
+    sub_status_ = nh_.subscribe<actionlib_msgs::GoalStatusArray>(
+	    "/move_base/status", 10, &GoalManager::navStatusCallBack, this);
+
+    // start timer
+    double interval_s = 0.0;
+    nh_.param("interval_s", interval_s, 5.0);
     timer_ = nh_.createTimer(
-	    ros::Duration(0.1),
+	    ros::Duration(interval_s),
 	    &GoalManager::timerCallback,
 	    this);
+
     return true;
 }
 
+void GoalManager::generateTarget(geometry_msgs::PoseStamped& target){
+    target.header.seq = 0;
+    target.header.stamp.sec = ros::Time::now().sec;
+    target.header.stamp.nsec = ros::Time::now().nsec;
+    target.header.frame_id = init_pose_.header.frame_id;
+    double theta_rad = (*unif_ptr_)(rnd_engine_);
+    target.pose.position.x = target_center_x_m_ + target_radius_m_*cos(theta_rad);
+    target.pose.position.y = target_center_y_m_ + target_radius_m_*sin(theta_rad);
+    target.pose.position.z = 0.0;
+    target.pose.orientation.x = 0.0;
+    target.pose.orientation.y = 0.0;
+    target.pose.orientation.z = sin(theta_rad/2.0);
+    target.pose.orientation.w = cos(theta_rad/2.0);
+}
+
 void GoalManager::timerCallback(const ros::TimerEvent& e){
-    printf("callbacked.\n");
+
+    // set initial pose of amcl
+    if (is_first_){
+	init_pose_.header.stamp.sec = ros::Time::now().sec;
+	init_pose_.header.stamp.nsec = ros::Time::now().nsec;
+	pub_init_pose_.publish(init_pose_);
+        ++init_pose_.header.seq;
+	ROS_INFO("Initial pose published.");
+        ROS_INFO("[x, y, w] = [%lf, %lf, %lf]\n",
+		init_pose_.pose.pose.position.x,
+                init_pose_.pose.pose.position.y,
+		init_w_deg_);
+	publishTarget();
+    }
+
+    double duration_s = ros::Time().now().sec - last_published_s_;
+    if ((nav_status_ == actionlib_msgs::GoalStatus::SUCCEEDED |
+         nav_status_ == actionlib_msgs::GoalStatus::PENDING |
+         duration_s > timeout_s_) && (duration_s > wait_after_new_target_s_)){
+	publishTarget();
+    }
+}
+
+void GoalManager::navStatusCallBack(const actionlib_msgs::GoalStatusArray::ConstPtr& status){
+    if (status->status_list.size() > 0){
+        nav_status_= status->status_list[0].status;
+	is_first_ = false;
+    }
+}
+
+void GoalManager::publishTarget(){
+    geometry_msgs::PoseStamped target;
+    generateTarget(target);
+    pub_target_.publish(target);
+    ROS_INFO("Target pose published.");
+    ROS_INFO("[x, y, w] = [%lf, %lf, %lf]\n",
+    	target.pose.position.x,
+            target.pose.position.y,
+    	acos(target.pose.orientation.w)*2/M_PI*180.0);
+    last_published_s_ = ros::Time().now().sec;
 }
 
 int main(int argc, char** argv){
